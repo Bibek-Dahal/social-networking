656d67d97a1c78a11e737e72caf19c2de56ab2c2
Bibek Dahal <bibekdahal479@gmail.com>
2024-06-06 16:35:32 +0545

    follow unfollow follow request completed

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,3 +1,4 @@
+import { FollowRequest } from "../models/followRequest.js";
 import { User } from "../models/user.js";
 export class UserController {
   static getLoggedInUser = (req, res) => {
@@ -24,115 +25,115 @@ export class UserController {
     }
   };
 
-  static followUser = async (req, res) => {
-    const { userId } = req.params;
-    try {
-      console.log(req.user);
-      const userToFollow = await User.findById(userId);
+  // static followUser = async (req, res) => {
+  //   const { userId } = req.params;
+  //   try {
+  //     console.log(req.user);
+  //     const userToFollow = await User.findById(userId);
 
-      if (userToFollow) {
-        //get followers of user to be followed
-        const followersOfUserToFollow = userToFollow.followers.map((item) =>
-          item.toString()
-        );
+  //     if (userToFollow) {
+  //       //get followers of user to be followed
+  //       const followersOfUserToFollow = userToFollow.followers.map((item) =>
+  //         item.toString()
+  //       );
 
-        if (followersOfUserToFollow.includes(req.user.id)) {
-          //if followers already includes current user id do not add follower
-          return res.status(400).send({
-            message: "Already in list of follower of following user",
-            success: false,
-          });
-        } else {
-          userToFollow.followers.push(req.user._id);
-          userToFollow.save();
-        }
+  //       if (followersOfUserToFollow.includes(req.user.id)) {
+  //         //if followers already includes current user id do not add follower
+  //         return res.status(400).send({
+  //           message: "Already in list of follower of following user",
+  //           success: false,
+  //         });
+  //       } else {
+  //         userToFollow.followers.push(req.user._id);
+  //         userToFollow.save();
+  //       }
 
-        const userCurrentFollowings = req.user.following.map((item) =>
-          item.toString()
-        );
-        console.log(userCurrentFollowings);
+  //       const userCurrentFollowings = req.user.following.map((item) =>
+  //         item.toString()
+  //       );
+  //       console.log(userCurrentFollowings);
 
-        if (userCurrentFollowings.includes(userId)) {
-          return res.status(400).send({
-            message: "Already following user",
-            success: false,
-          });
-        } else {
-          userCurrentFollowings.push(userToFollow._id);
-          const userToBeUpdated = req.user;
+  //       if (userCurrentFollowings.includes(userId)) {
+  //         return res.status(400).send({
+  //           message: "Already following user",
+  //           success: false,
+  //         });
+  //       } else {
+  //         userCurrentFollowings.push(userToFollow._id);
+  //         const userToBeUpdated = req.user;
 
-          userToBeUpdated.following = userCurrentFollowings;
-          userToBeUpdated.save();
-          return res.status(200).send({
-            message: "follower added successfully",
-            success: true,
-          });
-        }
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500).send({
-        message: "something went wrong",
-        success: false,
-      });
-    }
-  };
+  //         userToBeUpdated.following = userCurrentFollowings;
+  //         userToBeUpdated.save();
+  //         return res.status(200).send({
+  //           message: "follower added successfully",
+  //           success: true,
+  //         });
+  //       }
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //     res.status(500).send({
+  //       message: "something went wrong",
+  //       success: false,
+  //     });
+  //   }
+  // };
 
-  static unfollowUser = async (req, res) => {
-    const { userId } = req.params;
-    try {
-      console.log(req.user);
-      const userToFollow = await User.findById(userId);
+  // static unfollowUser = async (req, res) => {
+  //   const { userId } = req.params;
+  //   try {
+  //     console.log(req.user);
+  //     const userToFollow = await User.findById(userId);
 
-      if (userToFollow) {
-        //get followers of user to be followed
-        const followersOfUserToFollow = userToFollow.followers.map((item) =>
-          item.toString()
-        );
+  //     if (userToFollow) {
+  //       //get followers of user to be followed
+  //       const followersOfUserToFollow = userToFollow.followers.map((item) =>
+  //         item.toString()
+  //       );
 
-        if (!followersOfUserToFollow.includes(req.user.id)) {
-          //if followers already includes current user id do not add follower
-          return res.status(400).send({
-            message: "User is not in follower list",
-            success: false,
-          });
-        } else {
-          const valueIndex = userToFollow.followers.indexOf(req.user._id);
-          userToFollow.followers.splice(valueIndex, 1);
-          userToFollow.save();
-        }
+  //       if (!followersOfUserToFollow.includes(req.user.id)) {
+  //         //if followers already includes current user id do not add follower
+  //         return res.status(400).send({
+  //           message: "User is not in follower list",
+  //           success: false,
+  //         });
+  //       } else {
+  //         const valueIndex = userToFollow.followers.indexOf(req.user._id);
+  //         userToFollow.followers.splice(valueIndex, 1);
+  //         userToFollow.save();
+  //       }
 
-        const userCurrentFollowings = req.user.following.map((item) =>
-          item.toString()
-        );
-        console.log(userCurrentFollowings);
+  //       const userCurrentFollowings = req.user.following.map((item) =>
+  //         item.toString()
+  //       );
+  //       console.log(userCurrentFollowings);
 
-        if (!userCurrentFollowings.includes(userId)) {
-          return res.status(400).send({
-            message: "Userid not in list of following",
-            success: false,
-          });
-        } else {
-          const currentIndex = userCurrentFollowings.indexOf(userToFollow._id);
-          userCurrentFollowings.splice(currentIndex, 1);
-          const userToBeUpdated = req.user;
+  //       if (!userCurrentFollowings.includes(userId)) {
+  //         return res.status(400).send({
+  //           message: "Userid not in list of following",
+  //           success: false,
+  //         });
+  //       } else {
+  //         const currentIndex = userCurrentFollowings.indexOf(userToFollow._id);
+  //         userCurrentFollowings.splice(currentIndex, 1);
+  //         const userToBeUpdated = req.user;
 
-          userToBeUpdated.following = userCurrentFollowings;
-          userToBeUpdated.save();
-          return res.status(200).send({
-            message: "following removed successfully",
-            success: true,
-          });
-        }
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500).send({
-        message: "something went wrong",
-        success: false,
-      });
-    }
-  };
+  //         userToBeUpdated.following = userCurrentFollowings;
+  //         userToBeUpdated.save();
+  //         return res.status(200).send({
+  //           message: "following removed successfully",
+  //           success: true,
+  //         });
+  //       }
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //     res.status(500).send({
+  //       message: "something went wrong",
+  //       success: false,
+  //     });
+  //   }
+  // };
 
   static getUserById = async (req, res) => {
     const { userId } = req.params;
@@ -154,6 +155,252 @@ export class UserController {
         data: user,
       });
     } catch (error) {
+      res.status(500).send({
+        message: "something went wrong",
+        success: false,
+      });
+    }
+  };
+
+  static followUnfollowRequest = async (req, res) => {
+    const { userId } = req.params;
+    try {
+      if (userId === req.user.id) {
+        return res.status(400).send({
+          success: false,
+          message: "Cant follow to own id",
+        });
+      }
+
+      const followreq = await FollowRequest.findOne({
+        sender: req.user.id,
+        receiver: userId,
+      });
+      if (req.user.following.includes(userId)) {
+        return res.status(400).send({
+          success: false,
+          message: "Already following user",
+        });
+      }
+      if (followreq) {
+        const deletedItem = await FollowRequest.deleteOne({
+          _id: followreq.id,
+        });
+        if (deletedItem) {
+          return res.status(200).send({
+            success: true,
+            message: "Follow request unsent",
+          });
+        } else {
+          return res.status(404).send({
+            success: true,
+            message: "Follow request not found",
+          });
+        }
+      } else {
+        await FollowRequest.create({
+          sender: req.user.id,
+          receiver: userId,
+        });
+        return res.status(200).send({
+          success: true,
+          message: "Follow request sent",
+        });
+      }
+    } catch (error) {
+      res.status(500).send({
+        message: "something went wrong",
+        success: false,
+      });
+    }
+  };
+
+  static listFollowRequest = async (req, res) => {
+    console.log("list follow called");
+    try {
+      const followRequests = await FollowRequest.find({
+        receiver: req.user.id,
+      })
+        .select("sender")
+        .populate({
+          path: "sender",
+          select: "userName email profile",
+          populate: { path: "profile", select: "bio avatar" },
+        });
+
+      res.status(200).send({
+        success: true,
+        data: followRequests,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        message: "something went wrong",
+        success: false,
+      });
+    }
+  };
+
+  static acceptFollowRequest = async (req, res) => {
+    const { userId } = req.params;
+    try {
+      const followReqObj = await FollowRequest.findOne({
+        sender: userId,
+        receiver: req.user.id,
+      });
+      if (!followReqObj) {
+        return res.status(404).send({
+          success: false,
+          message: "No follow request exists",
+        });
+      }
+
+      const user = req.user;
+
+      user.followers.push(userId);
+      await user.save();
+      const followRequstingUser = await User.findById(userId);
+      if (!this.followUnfollowRequest) {
+        return res.status(404).send({
+          success: false,
+          message: "User not found",
+        });
+      }
+      followRequstingUser.following.push(req.user.id);
+      await followRequstingUser.save();
+      await FollowRequest.deleteOne({ _id: followReqObj.id });
+      return res.status(200).send({
+        success: true,
+        message: "Follow request accepted",
+      });
+    } catch (error) {
+      res.status(500).send({
+        message: "something went wrong",
+        success: false,
+      });
+    }
+  };
+
+  static deleteFollowRequest = async (req, res) => {
+    const { userId } = req.params;
+    try {
+      const followReqObj = await FollowRequest.findOne({
+        sender: userId,
+        receiver: req.user.id,
+      });
+      if (!followReqObj) {
+        return res.status(404).send({
+          success: false,
+          message: "No follow request exists",
+        });
+      }
+
+      await FollowRequest.deleteOne({ _id: followReqObj.id });
+      res.status(200).send({
+        success: true,
+        message: "Follow request unsent",
+      });
+    } catch (error) {
+      res.status(500).send({
+        message: "something went wrong",
+        success: false,
+      });
+    }
+  };
+
+  static listUserToFollow = async (req, res) => {
+    try {
+      const users = await User.find({
+        $and: [
+          { _id: { $ne: req.user.id } },
+          { _id: { $nin: req.user.following } },
+        ],
+      });
+      return res.status(200).send({
+        success: true,
+        data: users,
+        message: "User fetched successfully",
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        message: "something went wrong",
+        success: false,
+      });
+    }
+  };
+
+  static unfollowUser = async (req, res) => {
+    const { userId } = req.params;
+    try {
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).send({
+          success: false,
+          message: "User not found",
+        });
+      }
+      if (!req.user.following.includes(userId)) {
+        return res.status(400).send({
+          success: false,
+          message: "Cant unfollow user. Not in following list",
+        });
+      }
+
+      req.user.following.splice(req.user.following.indexOf(userId), 1);
+      await req.user.save();
+      res.status(200).send({
+        success: true,
+        message: "User unfollowed",
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        message: "something went wrong",
+        success: false,
+      });
+    }
+  };
+
+  static listUserFollowing = async (req, res) => {
+    try {
+      const users = await User.find(
+        {
+          _id: { $in: req.user.following },
+        },
+        { password: 0 }
+      ).populate("profile");
+      console.log(users);
+      res.status(200).send({
+        message: "Followings fetched successfully",
+        success: true,
+        data: users,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        message: "something went wrong",
+        success: false,
+      });
+    }
+  };
+
+  static listUserFollowers = async (req, res) => {
+    try {
+      const users = await User.find(
+        {
+          _id: { $in: req.user.followers },
+        },
+        { password: 0 }
+      ).populate("profile");
+      console.log(users);
+      res.status(200).send({
+        message: "Followers fetched successfully",
+        success: true,
+        data: users,
+      });
+    } catch (error) {
+      console.log(error);
       res.status(500).send({
         message: "something went wrong",
         success: false,
--- src/routes/user.js
+++ src/routes/user.js
@@ -6,7 +6,22 @@ const router = express.Router();
 // router.use(passport.authenticate("jwt", { session: false }));
 router.use(authMiddleware);
 router.get("/me", [UserController.getLoggedInUser]);
-router.get("/:userId", [UserController.getUserById]);
-router.post("/follow/:userId", UserController.followUser);
+router.get("/list-follow-request", UserController.listFollowRequest);
+router.post(
+  "/accept-follow-request/:userId",
+  UserController.acceptFollowRequest
+);
+router.post(
+  "/delete-follow-request/:userId",
+  UserController.deleteFollowRequest
+);
+router.get("/list-user-to-follow", UserController.listUserToFollow);
+router.post("/follow/:userId", UserController.followUnfollowRequest);
 router.post("/unfollow/:userId", UserController.unfollowUser);
+router.get("/list-user-following", UserController.listUserFollowing);
+router.get("/list-user-follower", UserController.listUserFollowers);
+router.get("/:userId", [UserController.getUserById]);
+
+// router.post("/unfollow/:userId", UserController.unfollowUser);
+
 export default router;
