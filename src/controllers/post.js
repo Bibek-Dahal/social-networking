caa645ff3b9bb811c5c42d0df96cbfc48a07cf4d
Bibek Dahal <bibekdahal479@gmail.com>
2024-07-24 15:09:46 +0545

    user like bool added in userListPos

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,5 +1,7 @@
+import mongoose from 'mongoose';
 import { Post } from '../models/post.js';
 import { eventEmitter } from '../utils/eventHandler.js';
+import { Schema } from 'mongoose';
 export class PostController {
   static createPost = async (req, res) => {
     try {
@@ -79,16 +81,73 @@ export class PostController {
 
   static listAllPosts = async (req, res) => {
     try {
-      const posts = await Post.find({ user: req.user.id }).populate(
-        'user',
-        'userName email followers following'
-      );
+      // Convert to string if needed
+      console.log(typeof req.user.id);
+      const userId = new mongoose.Types.ObjectId(`${req.user.id}`);
+      // const posts = await Post.find({ user: req.user.id }).populate(
+      //   'user',
+      //   'userName email followers following'
+      // );
+      // console.log('posts===', posts);
+
+      const posts = await Post.aggregate([
+        {
+          $match: {
+            user: userId,
+          },
+        },
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'user',
+            foreignField: '_id',
+            as: 'user',
+          },
+        },
+        {
+          $addFields: {
+            user: { $arrayElemAt: ['$user', 0] }, // Take the first (and only) element from user array
+          },
+        },
+        {
+          $lookup: {
+            from: 'likes',
+            let: { postId: '$_id' },
+            pipeline: [
+              {
+                $match: {
+                  $expr: {
+                    $and: [
+                      { $eq: ['$post', '$$postId'] }, // Match likes for the current post
+                      { $eq: ['$user', req.user._id] }, // Match likes by the current user
+                    ],
+                  },
+                },
+              },
+            ],
+            as: 'likes',
+          },
+        },
+        {
+          $addFields: {
+            likedByCurrentUser: { $gt: [{ $size: '$likes' }, 0] }, // Check if there are any likes (user has liked)
+          },
+        },
+        {
+          $project: {
+            'user.password': 0,
+            likes: 0, // Exclude likes array from the final output if not needed
+            // Add other fields to project if needed
+          },
+        },
+      ]);
       return res.status(200).send({
         message: 'Post fetched successfully',
         data: posts,
         success: true,
       });
     } catch (error) {
+      console.log('error', error);
       res.status(500).send({
         success: false,
         message: 'Something went wrong',
